dd350ab1aed5a44947c5a66e5fd20204bc0d2ec9
kajal kohli <kohlikajal00@gmail.com>
2025-08-16 16:00:22 +0530

    app.css solve

--- cromtech/src/App.js
+++ cromtech/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './app.css'
+import './App.css';
 import {BrowserRouter,Routes,Route} from'react-router-dom';
 import Home from './pages/home/Home';
 import About from'./pages/aboutus/About';
